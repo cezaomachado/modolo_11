94c28ef55e22277ec4236584026ff5e4d2066a88
Cezao Machado <cezaomachado@outlook.com>
2024-04-17 18:08:09 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,11 @@
+// Salve este código como scripts.js
+function adicionarAoCarrinho(botao) {
+    // Implemente a lógica para adicionar o produto ao carrinho
+    console.log('Produto adicionado ao carrinho!');
+  }
+// Adicione esta função no seu scripts.js
+function abrirCarrinho() {
+    // Implemente a lógica para abrir o carrinho
+    console.log('Carrinho aberto!');
+  }
+    
